25a59e067567d3a5967a9ff6374d02875c8fd634
OksanaFedyukova <avseniya222@gmail.com>
2023-03-22 13:44:50 +0100

    readme  - index

--- my-react-tasks/src/App.js
+++ my-react-tasks/src/App.js
@@ -1,21 +1,12 @@
 
 import React from 'react';
-import { Route, BrowserRouter, Routes } from 'react-router-dom';
-import Home from './views/Home/Home';
-import NotFoundPage from './views/NotFoundPage/NotFoundPage';
+
 
  
 function App() {
   return (
     <div>
-
-      <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<Home/>} />
-            <Route path='*' element={<NotFoundPage/>} />
-         </Routes>
-      </BrowserRouter>
-
+     REACT
     </div>
   )
 }
--- my-react-tasks/src/index.js
+++ my-react-tasks/src/index.js
@@ -2,15 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ThemeProvider } from '@mui/material';
-import theme from './styles/index'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
       <App />
-    </ThemeProvider>
   </React.StrictMode>
 );
 
